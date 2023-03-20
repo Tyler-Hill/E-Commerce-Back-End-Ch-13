fa635236ac8485943816923d79f359bdea8a5655
Tyler <hilltyler49@gmail.com>
2023-03-20 03:21:54 -0500

    fixed bug in tag-routes that prevented single ID get routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -28,25 +28,30 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    include: [{ model: Category }, { model: Tag }],
-  }).then((dbTagData) => {
-    if (!dbTagData) {
-      res.status(404).json({ message: "No Tag found with this id" });
-      return;
-    }
-    res.json(dbTagData);
-  });
+    include: [{ model: Product, attributes: ["product_name", "price", "stock", "category_id"] }],
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No Tag found with this id" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
   // create a new tag
   Tag.create(req.body)
     .then((tag) => {
-      if (req.body.tagIds.length) {
-        const tagIdArr = req.body.tagIds.map((tag_id) => {
+      if (req.body.productIds.length) {
+        const tagIdArr = req.body.productIds.map((product_id) => {
           return {
-            tag_id,
-            product_id: tag.id,
+            product_id,
+            tag_id: tag.id,
           };
         });
         return ProductTag.bulkCreate(tagIdArr);
@@ -70,16 +75,16 @@ router.put("/:id", (req, res) => {
       return ProductTag.findAll({ where: { tag_id: req.params.id } });
     })
     .then((productTags) => {
-      const productTagIds = productTags.map(({ product_id }) => product_id);
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
+      const productIds = productTags.map(({ product_id }) => product_id);
+      const newProductTags = req.body.productIds
+        .filter((product_id) => !productIds.includes(product_id))
+        .map((product_id) => {
           return {
-            tag_id,
-            product_id: req.params.id,
+            product_id,
+            tag_id: req.params.id,
           };
         });
-      const productTagsToRemove = productTags.filter(({ product_id }) => !req.body.tagIds.includes(product_id)).map(({ id }) => id);
+      const productTagsToRemove = productTags.filter(({ product_id }) => !req.body.productIds.includes(product_id)).map(({ id }) => id);
 
       return Promise.all([ProductTag.destroy({ where: { id: productTagsToRemove } }), ProductTag.bulkCreate(newProductTags)]);
     })
@@ -90,7 +95,7 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-  // delete on tag by its `id` value
+  // delete one tag by its `id` value
   Tag.destroy({
     where: {
       id: req.params.id,
